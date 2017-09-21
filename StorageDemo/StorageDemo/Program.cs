731e74204179df4a658044d86022634467398a6a
Piotr Wlodek <piotr.wlodek@avanade.com>
2017-09-21 08:29:39 +0200

    work

---
+++ StorageDemo/StorageDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StorageDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var demo = new GettingStartedQueues();
+            //demo.RunQueueStorageOperationsAsync();
+            //StorageBlobDemo.CallBlobGettingStartedSamples();
+            StorageTableDemo.RunSamples();
+            Console.WriteLine("End demo.");
+            Console.ReadKey();
+        }
+    }
+}
