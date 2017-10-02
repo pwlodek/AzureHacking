14cf82c73afa2c19d420fbc41269705fdbb085a8
Piotr Wlodek <piotr.wlodek@gmail.com>
2017-10-02 16:03:21 +0200

    updated event receiver

--- Messaging/ServiceBus/ServiceBus.Consumer/EventReceiver.cs
+++ Messaging/ServiceBus/ServiceBus.Consumer/EventReceiver.cs
@@ -2,6 +2,7 @@
 using Microsoft.ServiceBus.Messaging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,44 +12,71 @@ namespace ServiceBus.Consumer
     class EventReceiver
     {
         private string _connectionString = "Endpoint=sb://testeventhub111.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=tAjPkAV2gM3/ewR55y7Fm+X+wQMhkB1EDW/7VH5dZ2E=";
+        private string _storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=teststorage111piotr;AccountKey=4qMf9Kat9odR+s9GEQsk4QYcHOiRoIRfhKd1BLU/3LZ1egBM4fjIeRysoNC6vbgBHIZi7lZJtbjgDDKE9tKrgg==;EndpointSuffix=core.windows.net";
         private string _eventHubName = "myeventhub";
-        private EventHubClient _client;
+
+        EventProcessorHost _eventProcessorHost;
 
         public EventReceiver()
         {
             // Create namespace client
             NamespaceManager namespaceClient = NamespaceManager.CreateFromConnectionString(_connectionString);
             namespaceClient.CreateEventHubIfNotExists(_eventHubName);
-            
-            _client = EventHubClient.CreateFromConnectionString(_connectionString, _eventHubName);
         }
 
         public void Receive()
         {
+            string eventProcessorHostName = Guid.NewGuid().ToString();
+            _eventProcessorHost = new EventProcessorHost(eventProcessorHostName, _eventHubName, EventHubConsumerGroup.DefaultGroupName, _connectionString, _storageConnectionString);
             
-            _client.GetDefaultConsumerGroup().RegisterProcessor<Processor>(new Lease(), null);
+            var options = new EventProcessorOptions();
+            options.ExceptionReceived += (sender, e) => { Console.WriteLine(e.Exception); };
+            _eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>(options).Wait();
         }
 
         public void Close()
         {
-            
-            _client.Close();
+            _eventProcessorHost.UnregisterEventProcessorAsync().Wait();
         }
     }
 
-    public class Processor : IEventProcessor
+    class SimpleEventProcessor : IEventProcessor
     {
-        public async Task CloseAsync(PartitionContext context, CloseReason reason)
+        Stopwatch checkpointStopWatch;
+
+        async Task IEventProcessor.CloseAsync(PartitionContext context, CloseReason reason)
         {
+            Console.WriteLine("Processor Shutting Down. Partition '{0}', Reason: '{1}'.", context.Lease.PartitionId, reason);
+            if (reason == CloseReason.Shutdown)
+            {
+                await context.CheckpointAsync();
+            }
         }
 
-        public async Task OpenAsync(PartitionContext context)
+        Task IEventProcessor.OpenAsync(PartitionContext context)
         {
+            Console.WriteLine("SimpleEventProcessor initialized.  Partition: '{0}', Offset: '{1}'", context.Lease.PartitionId, context.Lease.Offset);
+            this.checkpointStopWatch = new Stopwatch();
+            this.checkpointStopWatch.Start();
+            return Task.FromResult<object>(null);
         }
 
-        public async Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        async Task IEventProcessor.ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
         {
-            Console.WriteLine("Event");
+            foreach (EventData eventData in messages)
+            {
+                string data = Encoding.UTF8.GetString(eventData.GetBytes());
+
+                Console.WriteLine(string.Format("Message received.  Partition: '{0}', Data: '{1}'",
+                    context.Lease.PartitionId, data));
+            }
+
+            //Call checkpoint every 5 minutes, so that worker can resume processing from 5 minutes back if it restarts.
+            if (this.checkpointStopWatch.Elapsed > TimeSpan.FromMinutes(5))
+            {
+                await context.CheckpointAsync();
+                this.checkpointStopWatch.Restart();
+            }
         }
     }
 }
